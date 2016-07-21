52d862c5ec1f9891f1d8850984fc5b199cf13561
ili <ili@WS-ILI1UA1>
2016-07-21 17:01:28 +0300

    Fix2

--- DbSearch/SearchDb.cs
+++ DbSearch/SearchDb.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Search;
 
 
 namespace DbSearch
 {
-    public class SearchDb
+    public class SearchDb:ISearch
     {
-        public static string Search()
+        /// <summary>
+        /// Search in Db
+        /// </summary>
+        /// <param name="str">What to search</param>
+        /// <param name="param">Param if needed (Connection string)</param>
+        /// <returns></returns>
+        public string Search(string str, string param = null)
         {
             return String.Empty;
         }
